dec5391d3af7d9e3c8bf21b61edf20d1c50f906e
JuanMaMujica <50682533+JuanMaMujica@users.noreply.github.com>
2022-04-21 17:43:39 -0300

    Dia13
    dia13 swagger y api users

---
+++ Dia13/challenge11-12/src/main/java/com/workshop/challenge1112/Challenge1112Application.java
@@ -0,0 +1,13 @@
+package com.workshop.challenge1112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge1112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge1112Application.class, args);
+	}
+
+}
